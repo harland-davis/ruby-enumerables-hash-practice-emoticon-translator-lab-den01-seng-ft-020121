30b8203bb56b00f7f1239e5e7d0b05a2a252094a
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-25 23:45:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon
 end
 
 
-def get_english_meaning(file_path, emoticon)
+def get_english_meaning(file_path, emoji)
   emoticons = load_library(file_path)
   binding.pry 
 end 
