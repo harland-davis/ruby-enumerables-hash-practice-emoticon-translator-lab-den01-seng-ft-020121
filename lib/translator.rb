b04f7f08dae3ccb1682211859bc2d741a65f3dd8
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-26 00:40:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ end
 def get_english_meaning(file_path, emoji)
   emoticons = load_library(file_path)
   emoticons.each do |word, value|
-    if emoji[word] == emoji[value]
+    if emoji[word].has_key?
       return word
       binding.pry 
     else 
