cd7e1fc66df2672808e08980e90bb3b13df1d0a7
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-26 01:31:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,8 +25,6 @@ def get_english_meaning(file_path, emoji)
   emoticons.each do |key, value|
       if value[:japanese] == emoji
         return key
-      else
-        'Sorry, that emoticon is not known.'
     end
   end
 end 
