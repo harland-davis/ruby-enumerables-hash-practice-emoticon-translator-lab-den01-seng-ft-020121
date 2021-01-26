91f50da5397656d0b929f4eb0f92f2b97d489fc2
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-26 01:35:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,9 +25,8 @@ def get_english_meaning(file_path, emoji)
   emoticons.each do |key, value|
       if value[:japanese] == emoji
         return key
-      else 
-        "Sorry, that emoticon was not found"
     end
   end
+  "Sorry, that emoticon was not found"
 end 
   
